f8420e2f10d257f9144d2cd2acafeeb5b8bac20b
DavidBabujyan <142348518+DavidBabujyan@users.noreply.github.com>
2023-11-23 06:16:34 -0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Resume from './title/title';
+
+
+function App() {
+  return (
+    <div className="App">
+     <Resume/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/title/title.jsx
@@ -0,0 +1,137 @@
+import React from "react";
+import "./title.css"
+
+function Resume() {
+    return ( 
+        <>
+<div className="all_div">
+<div className="center">
+<div className="title">
+<div className="text">
+    <div>
+
+<h3 className="div">Resume</h3>
+<h1 className="div">David</h1>
+<h1 className="div">Babujyan</h1>
+    </div>
+<div className="My_img">
+
+</div>
+</div>
+</div>
+<div className="hr">
+
+</div>
+<div className="div2">
+
+<div className="information">
+<h3>CONTACT INFORMATION</h3>
+{/* ________________________________ */}
+<div className="Mail txt">
+<div className="img">
+
+</div>
+<div className="Mail_text">
+<h4>davidbabuj64@gmail.com</h4>
+</div>
+</div>
+{/* _________________________________ */}
+<div className="Home txt">
+<div className="home_img">
+
+</div>
+<div className="Home_text">
+<h4>38,Yerevan 0054</h4>
+</div>
+</div>
+{/* ________________________________ */}
+<div className="phoneNamber txt">
+<div className="Phone_img">
+
+</div>
+<div className="Namber">
+<h4>(+374) 43-50-92-50</h4>
+</div>
+</div>
+{/* ________________________________ */}
+<div className="calendar txt">
+<div className="calendar_img">
+
+</div>
+<div className="calendar_text">
+<h4>Apr 28, 2008</h4>
+</div>
+</div>
+{/* ________________________________ */}
+<div className="Map txt">
+<div className="Map_img">
+
+</div>
+<div className="Map_text">
+<h4>Armenia</h4>
+</div>
+</div>
+{/* ________________________________ */}
+<div className="inst txt">
+<div className="inst_img">
+
+</div>
+<div className="inst_link">
+<h4><a href="https://instagram.com/babujyan.7_7_7_?igshid=OGQ5ZDc2ODk2ZA==" className="babujyan">My Instagram</a></h4>
+</div>
+</div>
+{/* ________________________________ */}
+<div className="hr2">
+
+</div>
+{/* ________________________________ */}
+<h3>SKILLS</h3>
+<ul>
+    <li>HTML</li>
+   <li>CSS</li>
+   <li>JavaScript (js)</li>
+   <li>REACT JS</li>
+</ul>
+{/* ________________________________ */}
+<div className="h3">
+
+</div>
+{/* ________________________________ */}
+<h3>LANGUAGES</h3>
+<ul>
+    <li>Armenia</li>
+    <li>English</li>
+    <li>Russian</li>
+</ul>
+ 
+</div>
+<div className="div1">
+
+</div>
+<div className="div_2">
+<h3 className="obj_title">OBJECTIVE</h3>
+<h5 className="obj_text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio perfdiverendis reiciendis atque incidunt quia ad sapiente totam assumenda? Debitis minima recusandae dolore ad tenetur dolorum soluta facilis! Accusantium, sequi consectetur.</h5>
+<div className="hr3">
+
+</div>
+<h3 className="experience">EXPERIENCE</h3>
+<h5 className="experience_text">I have no experience with</h5>
+<div className="hr3">
+
+</div>
+<div className="education">
+
+<h3 className="">EDUCATION</h3>
+<h5>I study at Samvel Gevorgyan High School No. 189</h5>
+</div>
+
+</div>
+
+</div>
+</div>
+</div>
+        </>
+     );
+}
+
+export default Resume;
